bbc03ed84a52ea48c11a51e656617bf730a576e9
Felipe Carvalho <felipeemanuelc12@gmail.com>
2025-05-21 22:43:29 -0300

    1768

---
+++ 1768/Solution.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public Class()
+{
+
+    public string MergeAlternately(string word1, string word2)
+    {
+        int largestWord = word1.Length > word2.Length
+            ? word1.Length
+            : word2.Length;
+
+        string output = "";
+
+        for (int i = 0; i < largestWord; i++)
+        {
+            if (i < word1.Length)
+            {
+                output += word1[i];
+            }
+
+            if (i < word2.Length)
+            {
+                output += word2[i];
+            }
+        }
+
+        return output;
+    }
+}
